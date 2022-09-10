0c6c418d120a6c4cbec8e2d87692fc97048df983
nitinkotania <nitin.kotania@gmail.com>
2022-09-11 02:53:39 +0530

    Update findMaxSubArrFixedSize.cpp

--- Practice/Array/findMaxSubArrFixedSize.cpp
+++ Practice/Array/findMaxSubArrFixedSize.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//Program to find Max Sub Array for Fixed Size sub array.
 
 #include <iostream>
 #include <climits>
